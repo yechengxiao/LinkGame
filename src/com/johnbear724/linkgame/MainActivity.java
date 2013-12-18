068a608bf28ea7e7a967e86e3fef90d93992fd0b
Johnbear724 <johnbear724@gmail.com>
2013-12-18 19:33:41 +0800

    Main UI

--- src/com/johnbear724/linkgame/MainActivity.java
+++ src/com/johnbear724/linkgame/MainActivity.java
@@ -2,10 +2,14 @@ package com.johnbear724.linkgame;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.view.KeyEvent;
 import android.view.Menu;
+import android.view.MotionEvent;
+import android.widget.TextSwitcher;
 
 public class MainActivity extends Activity {
 
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,5 +22,16 @@ public class MainActivity extends Activity {
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
-
+    
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        // TODO Auto-generated method stub
+        return super.onTouchEvent(event);
+    }
+    
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        // TODO Auto-generated method stub
+        return super.onKeyDown(keyCode, event);
+    }
 }
